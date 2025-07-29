f4bd67db3e1f8c7b0aad80075e7339f0ce61b53f
aungkyawth3t <aungkyawthethimself@gmail.com>
2025-07-29 16:12:20 +0630

    dynamic data

--- src/App.js
+++ src/App.js
@@ -5,15 +5,27 @@ import TodoList from './components/TodoList.js';
 import CheckAllAndRemaining from './components/CheckAllAndRemaining.js';
 import FilterButtons from './components/FilterButtons.js';
 import ClearCompletedButton from './components/ClearCompletedButton';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  let [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/todos')
+      .then(res => res.json())
+      .then((todo) => {
+        setTodos(todo);
+      })
+  }, [])
+
   return (
     <div className="todo-app-container">
       <div className="todo-app">
         <h2>Todo App</h2>
 
         <TodoForm/>
-        <TodoList/>
+        <TodoList todos={ todos } />
         <CheckAllAndRemaining/>
 
         {/* TodoFilter */}
