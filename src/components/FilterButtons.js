4ab6b52bc51189710683fe6c3d57b823f1f65ed0
aungkyawth3t <aungkyawthethimself@gmail.com>
2025-08-10 21:16:09 +0630

    filter

--- src/components/FilterButtons.js
+++ src/components/FilterButtons.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function FilterButtons() {
+
+  let [filter, setFilter] = useState('All');
   return (
     <div>
-        <button className="button filter-button filter-button-active">
-            All
-        </button>
-        <button className="button filter-button">Active</button>
-        <button className="button filter-button">Completed</button>
+      <button className={`button filter-button ${filter === 'All' ? 'filter-button-active' : ''}`} onClick={setFilter('All')}>
+        All
+      </button>
+      <button className={`button filter-button ${filter === 'Active' ? 'filter-button-active' : ''}`} onClick={setFilter('Active')}>Active</button>
+      <button className={`button filter-button ${filter === 'Completed' ? 'filter-button-active' : ''}`} onClick={setFilter('Completed')}>Completed</button>
     </div>
   )
 }
